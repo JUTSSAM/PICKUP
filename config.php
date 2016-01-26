af0589720cfa9adcc99b337d2190436e40669446
jut33am <jutssam@gmail.com>
2016-01-26 20:49:15 +0800

    initial commit

---
+++ config.php
@@ -0,0 +1,20 @@
+<?php
+
+
+
+$_PICK = array();
+$_PICK['db.server'] = SAE_MYSQL_HOST_M;
+$_PICK['db_database'] = 'app_jut33amdemo';
+$_PICK['db_user'] = SAE_MYSQL_USER;
+$_PICK[ 'db_password' ] = SAE_MYSQL_PASS;
+$_PICK[ 'ClassMenu' ] = 'datas';
+$_PICK['StudentsTable'] = 'Stu';
+
+
+?>
+
+
+
+
+
+
