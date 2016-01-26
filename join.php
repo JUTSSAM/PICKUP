b3f3b46d51cf6d0fe4a0320b2d6fa359361222c7
jut33am <jutssam@gmail.com>
2016-01-26 22:10:46 +0800

    initial commit

--- join.php
+++ join.php
@@ -2,7 +2,7 @@
 
 require_once 'config.php';
 
-if(!@mysql_connect($_PICK['db_server'].':'.$_PICK['db_port'],$_PICK['db_user'],$_PICK['db_password'])||!@mysql_select_db($_PICK['db_database']))
+if(!@$con=mysql_connect($_PICK['db_server'].':'.$_PICK['db_port'],$_PICK['db_user'],$_PICK['db_password'])||!@mysql_select_db($_PICK['db_database']))
 {
         die('数据库连接失败<br>');
 }else{
