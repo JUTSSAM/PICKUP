ceabb099e4f3c69fe38907732c162e3cdf4547e9
jut33am <jutssam@gmail.com>
2016-01-26 22:38:31 +0800

    initial commit

--- checkup.php
+++ checkup.php
@@ -1,9 +1,17 @@
 <?php
+	require_once 'config.php';
 
-	$con = mysql_connect($_PICK['db_server'].':'.$_PICK['db_port'];,$_PICK['db_user'],$_PICK['db_password']);
+	$con = mysql_connect($_PICK[ 'db_server' ].':'.$_PICK['db_port'],$_PICK[ 'db_user' ],$_PICK[ 'db_password']);
 	$db_selected = mysql_select_db($_PICK[ 'db_database' ], $con);
-	$consult = mysql_query("SELECT stuid,stuname FROM `stu` where keyword='".$_POST['keyword']."'",$con);
-
+	mysql_query("set names utf8");
 	
+	$consult = mysql_query("SELECT stuid,stuname FROM `stu` where keyword='".$_POST['keyword']."'",$con);
+	while ( $result = mysql_fetch_array($consult)) {
+		 // foreach ($result as $data) {
+			// echo $data;		
+			// }
+			print_r($result);
+		}
+
 
 ?>
