6357ccce73e253cd19779e4d41f6bffac26e9dc5
jut33am <jutssam@gmail.com>
2016-01-26 21:07:25 +0800

    initial commit

--- config.php
+++ config.php
@@ -3,8 +3,9 @@
 
 
 $_PICK = array();
-$_PICK['db.server'] = SAE_MYSQL_HOST_M;
-$_PICK['db_database'] = 'app_jut33amdemo';
+$_PICK['db_server'] = SAE_MYSQL_HOST_M;
+$_PICK['db_database'] = SAE_MYSQL_DB;
+$_PICK['db_port'] = SAE_MYSQL_PORT;
 $_PICK['db_user'] = SAE_MYSQL_USER;
 $_PICK[ 'db_password' ] = SAE_MYSQL_PASS;
 $_PICK[ 'ClassMenu' ] = 'datas';
--- setnew.php
+++ setnew.php
@@ -7,7 +7,9 @@
 require_once 'index.php';
 require_once 'config.php';
 
-if(!@mysql_connect($_PICK[ 'db_server' ],$_PICK[ 'db_user' ],$_PICK[ 'db_password'])||!@mysql_select_db($_PICK['db_database']))
+$con = mysql_connect(SAE_MYSQL_HOST_M.':'.SAE_MYSQL_PORT,SAE_MYSQL_USER,SAE_MYSQL_PASS);if(!$con){ die('could not connect:'.mysql_error()); }mysql_select_db(SAE_MYSQL_DB,$con);
+
+if(!@mysql_connect($_PICK['db_server'].':'.$_PICK['db_port'],$_PICK['db_user'],$_PICK['db_password'])||!@mysql_select_db($_PICK['db_database']))
 {
 	die('database connect error');
 }else{
