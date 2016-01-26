d27fe6583311eda660123f62504db8bfbb125f04
jut33am <jutssam@gmail.com>
2016-01-26 20:52:54 +0800

    initial commit

--- setnew.php
+++ setnew.php
@@ -5,7 +5,14 @@
 
 
 require_once 'index.php';
+require_once 'config.php';
 
+if(!@mysql_connect($_PICK[ 'db_server' ],$_PICK[ 'db_user' ],$_PICK[ 'db_password'])||!@mysql_select_db($_PICK['db_database']))
+{
+	die('database connect error');
+}else{
+	echo "数据库连接成功<br>";
+	}
 
 
 
