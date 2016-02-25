e6c574dfd9762f02b6efa6fd69244219a40df0e1
jut33am <jutssam@gmail.com>
2016-02-25 21:46:36 +0800

    better

--- appjoin.php
+++ appjoin.php
@@ -25,16 +25,18 @@ if($row){
     		'className' => $row[1]
     		)
     	);
-    	$strr = json_encode($arr);	
-	echo $strr;
+    	//$strr = json_encode($arr);	
+	//echo $strr;
+	var_dump($arr);
 	}else{
 	$arr = (
 		'code'=>0,
 		'msg'=>'点名失败请重试'
 		)
-	$strr = json_encode($arr);
-	echo $strr;
-	}
+	// $strr = json_encode($arr);
+	// echo $strr;
+	var_dump($arr);
+	 }
 }else{
 	die("Wrong input!");
 	
