cce81d0b9d5289ee85bd9b8e0af624a6aeb49bc8
jut33am <jutssam@gmail.com>
2016-02-27 21:32:00 +0800

    better

--- appjoin.php
+++ appjoin.php
@@ -11,12 +11,12 @@ $row = mysql_fetch_array($consult);
 $stuid = $_POST['stuId'];
 $stuname = $_POST['stuName'];
 $keyword = $_POST['keyword'];
-var_dump($stuid);
 if($row){
     // echo "请核对信息<br>";
     // //echo $row[0]."|".$row[1]."|".$row[2]."<br>";
     // echo "课程名称：".$row[1]."<br>"."教师姓名：".$row[2]."<br>";
   	$result = mysql_query("INSERT INTO `stu`(`stuid`, `Stuname`, `keyword`) VALUES ($stuid,'$stuname',$keyword);");
+  	var_dump($result);
 	if ($result){
 	$arr = array(
     	"code" => 1, 
