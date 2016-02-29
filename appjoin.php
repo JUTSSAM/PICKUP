d14fb83536dde0fbc1e64c4332f90e4b3944fcb7
jut33am <jutssam@gmail.com>
2016-02-29 13:17:53 +0800

    better

--- appjoin.php
+++ appjoin.php
@@ -7,30 +7,29 @@ $db_selected = mysql_select_db($_PICK[ 'db_database' ], $con);
 mysql_query("set names utf8");
 
 $consult_keyword = mysql_query("SELECT datetime,classname,tname  FROM `datas` WHERE keyword='".$_POST['keyword']."'",$con);
-$row = mysql_fetch_array($consult_keyword);
+$class_info = mysql_fetch_array($consult_keyword);
 	$stuid = $_POST['stuId'];
+	$nfcid = $_POST['nfcId']
 	$stuname = $_POST['stuName'];
 	$keyword = $_POST['keyword'];
-	var_dump($stuid);
-	var_dump($stuname);
-	var_dump($keyword);
+//	
 if (!($stuid&&$stuname&&$keyword)) {
 	$arr = array(
 		"code"=>0,
 		"msg"=>"输入有误，请检查！");
 	$strr = json_encode($arr);
 	echo $strr;		
-}else
-{
-if($row){
+}else{
+
+if($class_info){
 	$result = mysql_query("INSERT INTO `stu`(`stuid`, `Stuname`, `keyword`) VALUES ('$stuid','$stuname',$keyword);");
 	if ($result){
 	$arr = array(
     	"code" => 1, 
     	"msg" =>"点名成功请核对信息",
     	"obj" => array(
-    		"tName" => $row[2] ,
-    		"className" => $row[1]
+    		"tName" => $class_info[2] ,
+    		"className" => $class_info[1]
     		)
     	);
     	$strr = json_encode($arr);	
