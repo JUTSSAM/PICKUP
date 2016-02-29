5f52ea919d7a07b4cb6dcc00a3da092cd3b3e588
jut33am <jutssam@gmail.com>
2016-02-29 11:08:11 +0800

    better

--- appjoin.php
+++ appjoin.php
@@ -6,17 +6,13 @@ $con = mysql_connect($_PICK[ 'db_server' ].':'.$_PICK['db_port'],$_PICK[ 'db_use
 $db_selected = mysql_select_db($_PICK[ 'db_database' ], $con);
 mysql_query("set names utf8");
 
-$consult = mysql_query("SELECT datetime,classname,tname  FROM `datas` WHERE keyword='".$_POST['keyword']."'",$con);
-$row = mysql_fetch_array($consult);
-$stuid = $_POST['stuId'];
-$stuname = $_POST['stuName'];
-$keyword = $_POST['keyword'];
+$consult_keyword = mysql_query("SELECT datetime,classname,tname  FROM `datas` WHERE keyword='".$_POST['keyword']."'",$con);
+$row = mysql_fetch_array($consult_keyword);
+	$stuid = $_POST['stuId'];
+	$stuname = $_POST['stuName'];
+	$keyword = $_POST['keyword'];
 if($row){
-    // echo "请核对信息<br>";
-    // //echo $row[0]."|".$row[1]."|".$row[2]."<br>";
-    // echo "课程名称：".$row[1]."<br>"."教师姓名：".$row[2]."<br>";
-  	$result = mysql_query("INSERT INTO `stu`(`stuid`, `Stuname`, `keyword`) VALUES ('$stuid','$stuname',$keyword);");
-  	//var_dump($result);
+	$result = mysql_query("INSERT INTO `stu`(`stuid`, `Stuname`, `keyword`) VALUES ('$stuid','$stuname',$keyword);");
 	if ($result){
 	$arr = array(
     	"code" => 1, 
@@ -28,7 +24,6 @@ if($row){
     	);
     	$strr = json_encode($arr);	
 	echo $strr;
-	//var_dump($arr);
 	}else{
 	$arr = array(
 		"code" => 0,
@@ -36,7 +31,6 @@ if($row){
 		);
 	$strr = json_encode($arr);
 	echo $strr;
-	//var_dump($arr);
 	}
 }else{
 	$arr = array(
