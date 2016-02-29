d798dce7f888269a33366f24e325da894d992fe9
jut33am <jutssam@gmail.com>
2016-02-29 19:33:07 +0800

    better

--- appjoin.php
+++ appjoin.php
@@ -10,7 +10,7 @@ $consult_keyword = mysql_query("SELECT datetime,classname,tname  FROM `datas` WH
 $class_info = mysql_fetch_array($consult_keyword);
 $consult_stu = mysql_query("SELECT `stu_id`,`nfc_id`,`stu_name`FROM `stu2` WHERE `nfc_id` ='".$_POST['nfcId']."'",$con);
 $stu_info = mysql_fetch_array($consult_stu);
-
+var_dump($stu_info);
 	$stuid = $_POST['stuId'];
 	$nfcid = $_POST['nfcId'];
 	$stuname = $_POST['stuName'];
