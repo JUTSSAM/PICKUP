b9330a576ada540ec50d21bc6721bf6442aa9c56
jut33am <jutssam@gmail.com>
2016-03-03 13:18:17 +0800

    better

--- join.php
+++ join.php
@@ -13,25 +13,28 @@ $con = mysql_connect($_PICK[ 'db_server' ].':'.$_PICK['db_port'],$_PICK[ 'db_use
 $db_selected = mysql_select_db($_PICK[ 'db_database' ], $con);
 mysql_query("set names utf8");
 
-$consult = mysql_query("SELECT datetime,classname,tname  FROM `datas` WHERE keyword='".$_POST['keyword']."'",$con);
+$consult = mysql_query("SELECT datetime,classname,tname  FROM `datas` WHERE keyword='".$_REQUEST['keyword']."'",$con);
 $row = mysql_fetch_array($consult);
-$stuid = $_POST['stuid'];
-$stuname = $_POST['stuname'];
-$keyword = $_POST['keyword'];
+$stuid = $_REQUEST['stuid'];
+$stuname = $_REQUEST['stuname'];
+$keyword = $_REQUEST['keyword'];
 
-if($row){
-    echo "请核对信息<br>";
-    //echo $row[0]."|".$row[1]."|".$row[2]."<br>";
-    echo "课程名称：".$row[1]."<br>"."教师姓名：".$row[2]."<br>";
-    
-    $result = mysql_query("INSERT INTO `stu`(`stuid`, `Stuname`, `keyword`) VALUES ($stuid,'$stuname',$keyword);");
-	if ($result){
-    	echo "签到成功！<br>";
-		}	
-}else{
-    die( "输入有误，请核对重新输入<br>");
+if (isset($_REQUEST['check'])) {
+	# code...
+
+	if($row){
+	    echo "请核对信息<br>";
+	    //echo $row[0]."|".$row[1]."|".$row[2]."<br>";
+	    echo "课程名称：".$row[1]."<br>"."教师姓名：".$row[2]."<br>";
+	    
+	    $result = mysql_query("INSERT INTO `stu`(`stuid`, `Stuname`, `keyword`) VALUES ($stuid,'$stuname',$keyword);");
+		if ($result){
+	    	echo "签到成功！<br>";
+			}	
+	}else{
+	    die( "输入有误，请核对重新输入<br>");
+	}
 }
-
 ?>
 <!DOCTYPE html>
 <html>
@@ -41,7 +44,7 @@ if($row){
 </head>
 <body>
 
-	<form action="?check" method="POST">
+	<form action="?check" method="REQUEST">
 		
 		学号:<input type="text" name="stuid" ><br>
 		姓名:<input type="text" name="stuname"><br>
