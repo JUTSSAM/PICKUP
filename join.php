4adf1dd8d99f7e112c48ec1ea7a5c49e48b363e0
jut33am <jutssam@gmail.com>
2016-03-07 05:31:45 +0800

    b

--- join.php
+++ join.php
@@ -12,7 +12,7 @@ require_once 'config.php';
 $con = mysql_connect($_PICK[ 'db_server' ].':'.$_PICK['db_port'],$_PICK[ 'db_user' ],$_PICK[ 'db_password']);
 $db_selected = mysql_select_db($_PICK[ 'db_database' ], $con);
 mysql_query("set names utf8");
-	//echo "<div id=\"info\">";
+	echo "<div id=\"info\">";
 	$consult = mysql_query("SELECT datetime,classname,tname  FROM `datas` WHERE keyword='".$_POST['keyword']."'",$con);
 	$row = mysql_fetch_array($consult);
 	$stuid = $_POST['stuid'];
@@ -36,6 +36,6 @@ mysql_query("set names utf8");
 	}else{
 	    die( "输入有误，请核对重新输入<br>");
 	}
-	//echo "</div>";
+	echo "</div>";
 
 ?>
