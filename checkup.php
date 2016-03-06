b30af4e813e1533c96c5820ee67a81b5b51fd16a
jut33am <jutssam@gmail.com>
2016-03-07 05:36:27 +0800

    b

--- checkup.php
+++ checkup.php
@@ -7,10 +7,10 @@
 		echo "<div id='link'><span id='test'>";
 		//var_dump($_POST['classname']);
 		//var_dump($_POST['classid']);
-		$consult_key = //mysql_query("SELECT keyword FROM 'datas' WHERE classname = '".$_POST['classname']."' AND classid = ".$_POST['classid'].";");
-		mysql_query("SELECT keyword FROM  `datas` WHERE classname =  '".$_POST['classname']."' AND classid =".$_POST['classid']);
+		$consult_key = mysql_query("SELECT keyword FROM  `datas` WHERE classname =  '".$_POST['classname']."' AND classid =".$_POST['classid']);
+		var_dump($consult_key);
 		$result_keyword = mysql_fetch_array($consult_key);
-		//echo $result_keyword['keyword'];
+		echo $result_keyword['keyword'];
 		$consult = mysql_query("SELECT DISTINCT stuid,stuname FROM `stu` where keyword=".$result_keyword['keyword'],$con);
 		echo "到课信息如下：<br>";
 		while ( $result = mysql_fetch_array($consult)) {
