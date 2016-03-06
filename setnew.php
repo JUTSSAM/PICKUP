5eaf13c920dfb026b79f8f227f6919522b24e07a
jut33am <jutssam@gmail.com>
2016-03-07 02:35:46 +0800

    better

--- setnew.php
+++ setnew.php
@@ -93,6 +93,7 @@ if(!@mysql_connect($_PICK['db_server'].':'.$_PICK['db_port'],$_PICK['db_user'],$
 	$keyword = rand(100000,999999);
 	$time = time();
 
+echo "hello world";
 	
 
 if (isset($_REQUEST['check'])) {
